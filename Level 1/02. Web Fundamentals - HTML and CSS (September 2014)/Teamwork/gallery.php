d63e931bf2f89243d91ac68789df1410eb14172c
Nikola Nikolov <nikola_00219@abv.bg>
2014-11-26 17:25:23 +0200

    github reorder

---
+++ Level 1/02. Web Fundamentals - HTML and CSS (September 2014)/Teamwork/gallery.php
@@ -0,0 +1,61 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link type="text/css" href="styles/style.css" rel="stylesheet"/>
+    <link type="text/css" href="styles/styles-gallery.css" rel="stylesheet"/>
+    <link rel="stylesheet" href="styles/baguetteBox.min.css">
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/global.js"></script>
+    <script src="js/baguetteBox.min.js" async></script>
+    <title>Галерия</title>
+</head>
+<body>
+<header class="header">
+    <?php include 'templates/header.php'; ?>
+</header>
+<main>
+    <div id="main-wrapper">
+        <section>
+            <div class="baguetteBoxOne gallery">
+                <div><a href="resources/images/gallery/2typesOfPeople.png"><img src="resources/images/gallery/2typesOfPeople.png" title="Натисни за по-голям размер" alt="image"></a></div>
+                <div><a href="resources/images/gallery/7-being-a-programmer-joke.jpg"><img src="resources/images/gallery/7-being-a-programmer-joke.jpg" title="Натисни за по-голям размер" alt="image"></a></div>
+                <div><a href="resources/images/gallery/10-awesome-programming-jokes.jpg"><img src="resources/images/gallery/10-awesome-programming-jokes.jpg" title="Натисни за по-голям размер" alt="image"></a></div>
+                <div><a href="resources/images/gallery/automation.png"><img src="resources/images/gallery/automation.png" title="Натисни за по-голям размер" alt="image"></a></div>
+
+                <div><a href="resources/images/gallery/chuck-norris-demotivational.jpg"><img src="resources/images/gallery/chuck-norris-demotivational.jpg" title="Натисни за по-голям размер" alt="image"></a></div>
+                <div><a href="resources/images/gallery/codeStyleEvolution.jpg"><img src="resources/images/gallery/codeStyleEvolution.jpg" title="Натисни за по-голям размер" alt="image"></a></div>
+                <div><a href="resources/images/gallery/compiling2.png"><img src="resources/images/gallery/compiling2.png" title="Натисни за по-голям размер" alt="image"></a></div>
+                <div><a href="resources/images/gallery/evolution.jpg"><img src="resources/images/gallery/evolution.jpg" title="Натисни за по-голям размер" alt="image"></a></div>
+
+                <div><a href="resources/images/gallery/free_lancer.jpg"><img src="resources/images/gallery/free_lancer.jpg" title="Натисни за по-голям размер" alt="image"></a></div>
+                <div><a href="resources/images/gallery/gradeC++.jpg"><img src="resources/images/gallery/gradeC++.jpg" title="Натисни за по-голям размер" alt="image"></a></div>
+                <div><a href="resources/images/gallery/homeSweetHome.jpg"><img src="resources/images/gallery/homeSweetHome.jpg" title="Натисни за по-голям размер" alt="image"></a></div>
+                <div><a href="resources/images/gallery/lol_programming.jpg"><img src="resources/images/gallery/lol_programming.jpg" title="Натисни за по-голям размер" alt="image"></a></div>
+
+                <div><a href="resources/images/gallery/MAoO4uT.jpg"><img src="resources/images/gallery/MAoO4uT.jpg" title="Натисни за по-голям размер" alt="image"></a></div>
+                <div><a href="resources/images/gallery/notSure.jpg"><img src="resources/images/gallery/notSure.jpg" title="Натисни за по-голям размер" alt="image"></a></div>
+                <div><a href="resources/images/gallery/programmer%20jokes.jpg"><img src="resources/images/gallery/programmer%20jokes.jpg" title="Натисни за по-голям размер" alt="image"></a></div>
+                <div><a href="resources/images/gallery/programmers-be-like.jpg"><img src="resources/images/gallery/programmers-be-like.jpg" title="Натисни за по-голям размер" alt="image"></a></div>
+
+                <div><a href="resources/images/gallery/random_number.png"><img src="resources/images/gallery/random_number.png" title="Натисни за по-голям размер" alt="image"></a></div>
+                <div><a href="resources/images/gallery/softwareProduct.jpg"><img src="resources/images/gallery/softwareProduct.jpg" title="Натисни за по-голям размер" alt="image"></a></div>
+                <div><a href="resources/images/gallery/stagesOfEveryTask.jpg"><img src="resources/images/gallery/stagesOfEveryTask.jpg" title="Натисни за по-голям размер" alt="image"></a></div>
+                <div><a href="resources/images/gallery/tan_lines_of_a_programmer2.jpg"><img src="resources/images/gallery/tan_lines_of_a_programmer2.jpg" title="Натисни за по-голям размер" alt="image"></a></div>
+
+                <div><a href="resources/images/gallery/WaterFallApproachInTheoryVSInPractice.jpg"><img src="resources/images/gallery/WaterFallApproachInTheoryVSInPractice.jpg" title="Натисни за по-голям размер" alt="image"></a></div>
+            </div>
+        </section>
+    </div>
+</main>
+<footer>
+    <?php include 'templates/footer.php'; ?>
+</footer>
+<script>
+    window.onload = function() {
+        baguetteBox.run('.baguetteBoxOne');
+    };
+</script>
+</body>
+</html>
---
+++ Level 1/02. Web Fundamentals - HTML and CSS (September 2014)/Teamwork/index.php
@@ -0,0 +1,50 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link type="text/css" href="styles/style.css" rel="stylesheet"/>
+    <link type="text/css" href="styles/styles-index.css" rel="stylesheet"/>
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/global.js"></script>
+    <title>Начало</title>
+</head>
+<body>
+<header class="header">
+    <?php include 'templates/header.php';?>
+</header>
+<main>
+    <div id="main-wrapper">
+        <h1 id="homeHeader">Добре дошли в нашата паралелна Вселена!</h1>
+        <h3 id="homeSubheader"> Тук можете да откриете всякакви нелепици от ежедневието на обитателите на SoftUni </h3>
+        <div id="links">
+            <a href="gallery.php" class="homeLinks">
+                <section class="homeSection">
+                    <h4>Галерия</h4>
+                    <img src="resources/images/home/gallery.jpg" alt="galleryImage" class="homeImgSection"/>
+                    <p class="homeParagraph">В нашата галерия ще откриете интересни картинки.</p>
+                </section>
+            </a>
+            <a href="guide.php" class="homeLinks">
+                <section class="homeSection">
+                    <h4>Наръчник</h4>
+                    <img src="resources/images/home/guide.jpg" alt="guideImage" class="homeImgSection"/>
+                    <p class="homeParagraph">Какъв е пътят на програмиста? </p>
+                </section>
+            </a>
+            <a href="jokes.php" class="homeLinks">
+                <section class="homeSection">
+                    <h4>Бисери</h4>
+                    <img src="resources/images/home/text.png" alt="textImage" class="homeImgSection"/>
+                    <p class="homeParagraph">В секцията с бисери сме ви подготвили всякакви тъпотии в писмен вид</p>
+                </section>
+            </a>
+            <h4 id="homePS">П.С. Ако не си от нашата Вселена е силно вероятно да не ни разбереш</h4>
+        </div>
+    </div>
+</main>
+<footer>
+    <?php include 'templates/footer.php';?>
+</footer>
+</body>
+</html>
