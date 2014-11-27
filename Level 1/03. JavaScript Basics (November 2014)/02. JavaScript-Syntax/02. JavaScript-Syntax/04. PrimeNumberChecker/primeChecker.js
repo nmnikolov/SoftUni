069a671d960975394bbb43491f0245c942b72063
Nikola Nikolov <nikola_00219@abv.bg>
2014-11-27 11:35:02 +0200

    primeChecker update

--- Level 1/03. JavaScript Basics (November 2014)/02. JavaScript-Syntax/02. JavaScript-Syntax/04. PrimeNumberChecker/primeChecker.js
+++ Level 1/03. JavaScript Basics (November 2014)/02. JavaScript-Syntax/02. JavaScript-Syntax/04. PrimeNumberChecker/primeChecker.js
@@ -1,19 +1,17 @@
 ﻿function isPrime(number) {
     if (number < 2) {
         return false;
-    } else if (number == 2) {
-        return true;
     } else {
         for (var i = 2; i <= Math.sqrt(number) ; i++) {
             if (number % i == 0) {
                 return false;
             }
-        }
+        }    
+    }
 
-        return true;
-    }   
+    return true;
 }
 
-console.log(isPrime(7));
+console.log(isPrime(2));
 console.log(isPrime(254));
 console.log(isPrime(587));
