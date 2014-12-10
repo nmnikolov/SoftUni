427c3a49628cb3e4ad374aa2bb5aa0a91e6843b6
Nikola Nikolov <nikola_00219@abv.bg>
2014-12-10 18:35:02 +0200

    05. Arrays-Strings-Objects

---
+++ Level 1/04. PHP Basics (November 2014)/05. Arrays-Strings-Objects/01. WordMapping/WordMapper.php
@@ -0,0 +1,31 @@
+<?php
+
+if (isset($_POST['text'])):
+
+    $words = str_word_count($_POST['text'], 2);
+    $wordsCount = array_count_values(array_map('strtolower', $words)); ?>
+
+    <p> <span>Text: </span> <?= htmlentities($_POST['text']) ?></p>
+
+    <?php if (empty($wordsCount)): ?>
+        <p>No words in the text.</p>
+    <?php else: ?>
+        <table>
+            <thead>
+                <tr>
+                    <th>Word</th>
+                    <th>Count</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($wordsCount as $key => $value): ?>
+                    <tr>
+                        <td> <?= $key; ?></td>
+                        <td> <?= $value; ?> </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    <?php endif;
+
+endif; ?>
---
+++ Level 1/04. PHP Basics (November 2014)/05. Arrays-Strings-Objects/01. WordMapping/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Word Mapping</title>
+    <link type="text/css" rel="stylesheet" href="styles/style.css">
+</head>
+<body>
+<div id="container">
+    <form action="" method="post">
+        <textarea name="text" id="text" required></textarea>
+        <input type="submit" id="submit" value="Submit" />
+    </form>
+
+<?php require 'WordMapper.php' ?>
+
+</div>
+</body>
+</html>
