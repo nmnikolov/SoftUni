4daf625c5c7f6ab9350e9abbc129a3adc0896ed0
Nikola Nikolov <nikola_00219@abv.bg>
2014-12-10 22:33:26 +0200

    minor change

--- Level 1/04. PHP Basics (November 2014)/05. Arrays-Strings-Objects/02. ColoringTexts/TextColorer.php
+++ Level 1/04. PHP Basics (November 2014)/05. Arrays-Strings-Objects/02. ColoringTexts/TextColorer.php
@@ -5,11 +5,7 @@ if (isset($_POST['text'])):
     $chars = str_split($string);
 
     foreach ($chars as $char):
-        if (ord($char) % 2 == 0): ?>
-            <span class="red"> <?= $char ?> </span>
-        <?php else: ?>
-            <span class="blue"> <?= $char ?> </span>
-        <?php endif;
-    endforeach;
-endif;
-?>
+        $color = ord($char) % 2 == 0 ? "red" : "blue"; ?>
+        <span class="<?= $color ?>"> <?= $char ?> </span>
+    <?php endforeach;
+endif; ?>
