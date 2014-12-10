1dc27602f462b8daeb8386b52e994164b9a96278
Nikola Nikolov <nikola_00219@abv.bg>
2014-12-10 22:14:32 +0200

    minor change

--- Level 1/04. PHP Basics (November 2014)/05. Arrays-Strings-Objects/01. WordMapping/index.php
+++ Level 1/04. PHP Basics (November 2014)/05. Arrays-Strings-Objects/01. WordMapping/index.php
@@ -8,7 +8,7 @@
 <div id="container">
     <form action="" method="post">
         <textarea name="text" id="text" required></textarea>
-        <input type="submit" id="submit" value="Submit" />
+        <input type="submit" id="submit" value="Count words" />
     </form>
 
 <?php require 'WordMapper.php' ?>
