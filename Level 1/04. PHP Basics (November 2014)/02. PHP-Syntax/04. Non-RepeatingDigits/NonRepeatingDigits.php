5a747ab3c4b327e5330531d9cd16c89708e83792
Nikola Nikolov <nikola_00219@abv.bg>
2014-12-10 22:14:51 +0200

    minor change

--- Level 1/04. PHP Basics (November 2014)/02. PHP-Syntax/04. Non-RepeatingDigits/NonRepeatingDigits.php
+++ Level 1/04. PHP Basics (November 2014)/02. PHP-Syntax/04. Non-RepeatingDigits/NonRepeatingDigits.php
@@ -1,11 +1,11 @@
 <?php
 
 function compare($var) {
-    $numbers = array();
+    $numbers = [];
     for ($i = 100; $i <= 1000; $i++) {
         $number = (string)$i;
         if($i <= $var && $number[0] != $number[1] && $number[1] != $number[2] && $number[0] != $number[2]) {
-            array_push($numbers, $i);
+            $numbers[] = $i;
         }
     }
 
