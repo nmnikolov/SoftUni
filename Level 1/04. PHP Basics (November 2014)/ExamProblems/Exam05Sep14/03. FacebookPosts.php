9b9c2b9b6f8c0021b72e6bcb48fde63411f04fb6
Nikola Nikolov <nikola_00219@abv.bg>
2015-01-11 22:48:34 +0200

    minor change

--- Level 1/04. PHP Basics (November 2014)/ExamProblems/Exam05Sep14/03. FacebookPosts.php
+++ Level 1/04. PHP Basics (November 2014)/ExamProblems/Exam05Sep14/03. FacebookPosts.php
@@ -1,48 +1,6 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Facebook Posts</title>
-    <style>
-        label, textarea {
-            display: block;
-        }
-        textarea {
-            width: 450px;
-            height: 150px;
-        }
-        form {
-            margin-bottom: 30px;
-        }
-        input[type="submit"] {
-            margin: 5px;
-        }
-        h2 {
-            margin: 0;
-        }
-    </style>
-<body>
-<h2>Zero Test #1</h2>
-<form action="" method="get">
-    <label for="text">Text:</label>
-    <textarea name="text">Pesho Meshev;2-9-2014;    Sometimes I post very deep messages on facebook that have nothing to do with who I am or what I do, but I like people to think that I'm very, very deep.    ;160;Confession of the century/hahaha this is so NOT funny</textarea>
-    <input type="submit" value="SUBMIT"/>
-</form>
-
-<h2>Zero Test #2</h2>
-<form action="" method="get">
-    <label for="text">Text:</label>
-    <textarea name="text">Pesho Meshev;2-9-2014;Sometimes I post very deep messages on facebook that have nothing to do with who I am or what I do, but I like people to think that I'm very, very deep; 160;  Confession of the century/hahaha this is so NOT funny
-
-Chico Tashko;06-06-2014;Zasqh piperite i jinite, vreme e za rakiq;2345;</textarea>
-    <input type="submit" value="SUBMIT"/>
-</form>
-</body>
-</html>
-
 <?php
 date_default_timezone_set('Europe/Sofia');
 $text = $_GET['text'];
-//$text = '%3Cp%3E%26alt%3C%2Fp%3E%3B15-6-2000%3BDadadadaadaad+++++++++++++%3B++++++++15++++++++++++%3B+++dawdawdwa+%2F+dlkajdkwlj+%2F+uu53092u5%2F+4uoidafjea%0D%0A%0D%0A';
 $rows = preg_split("/\n/", $text, -1, PREG_SPLIT_NO_EMPTY);
 
 for ($row = 0; $row < count($rows); $row++) {
