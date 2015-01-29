bed07c99e28db8f93223ce81cf6dde9772ed21e2
Nikola Nikolov <nikola_00219@abv.bg>
2015-01-29 22:24:41 +0200

    add Exception Handling homework

---
+++ Level 2/01. OOP (January 2015)/02.2 ExceptionHandling/01. SquareRoot/SquareRoot.cs
@@ -0,0 +1,37 @@
+﻿namespace SquareRoot
+{
+    using System;
+
+    class SquareRoot
+    {
+        static void Main()
+        {
+            try
+            {
+                long number = long.Parse(Console.ReadLine());
+                if (number < 0)
+                {
+                    throw new ArgumentOutOfRangeException("number", "Number cannot be negative");
+                }
+
+                Console.WriteLine("Square root: {0}", Math.Sqrt(number));
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine("Invalid number");
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                Console.WriteLine("Good bye.");
+            }
+        }
+    }
+}
---
+++ Level 2/01. OOP (January 2015)/02.2 ExceptionHandling/02. EnterNumbers/NumbersEnterer.cs
@@ -0,0 +1,59 @@
+﻿namespace Numbers
+{
+    using System;
+    using System.Linq;
+
+    class NumbersEnterer
+    {
+        public static void Main()
+        {
+            int[] userNumbers = new int[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                int start = Math.Max(1, userNumbers.Max());
+                int end = 91 + i;
+                userNumbers[i] = ReadNumber(start, end);
+            }
+
+            Console.WriteLine("\nEntered numbers:");
+            for (int i = 0; i < userNumbers.Length; i++)
+            {
+                Console.WriteLine("Number[{0}]: {1}", i + 1, userNumbers[i]);
+            }
+        }
+
+        public static int ReadNumber(int start, int end)
+        {
+            try
+            {
+                Console.Write("Enter number [{0}...{1}]: ", start + 1, end - 1);
+                int number = int.Parse(Console.ReadLine());
+                if (number <= start || number >= end)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+
+                return number;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("You should enter integer number. Try again:");
+
+                return ReadNumber(start, end);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Number should be in the range [{0}...{1}]. Try again:", start + 1, end - 1);
+
+                return ReadNumber(start, end);
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine(ex.Message + " Try again:");
+
+                return ReadNumber(start, end);
+            }
+        }
+    }
+}
