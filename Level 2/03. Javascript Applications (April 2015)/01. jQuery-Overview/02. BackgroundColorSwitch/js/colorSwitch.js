c9b0cd83329423cf919152358f83364652e3a073
Nikola Nikolov <nikola_00219@abv.bg>
2015-04-01 02:24:11 +0300

    add 01. jQuery-Overview

---
+++ Level 2/03. Javascript Applications (April 2015)/01. jQuery-Overview/02. BackgroundColorSwitch/js/colorSwitch.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function () {
+    var classes = [];
+
+    $('ul').children().each(function (index, el) {
+        if ($(el).attr('class')) {
+            classes.push($(el).attr('class'));
+        }
+    });
+
+    $(classes).each(function(index, el) {
+        $('#classes').append('<option value="' + el + '">' + el + '</option>');
+    });
+
+    $('button').on('click', function () {
+        var className = '.' + $("#classes").val();
+        var color = $('#color').val();
+        $(className).css('background', color);
+    });
+});
