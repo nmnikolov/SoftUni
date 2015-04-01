26dd023a855bf2cb1a093f11a1d0139e7ed4d8f3
Nikola Nikolov <nikola_00219@abv.bg>
2015-04-01 23:02:46 +0300

    add 03. TableGenerator

--- Level 2/03. Javascript Applications (April 2015)/01. jQuery-Overview/02. BackgroundColorSwitch/js/colorSwitch.js
+++ Level 2/03. Javascript Applications (April 2015)/01. jQuery-Overview/02. BackgroundColorSwitch/js/colorSwitch.js
@@ -7,13 +7,28 @@
         }
     });
 
+    $('#available-classes').text($('#available-classes').text() + classes.join(', '));
+
     $(classes).each(function(index, el) {
-        $('#classes').append('<option value="' + el + '">' + el + '</option>');
+        $('#classes-select').append('<option value="' + el + '">' + el + '</option>');
     });
 
-    $('button').on('click', function () {
-        var className = '.' + $("#classes").val();
-        var color = $('#color').val();
+    $('.paint-button:eq(0)').click(function () {
+        var className = '.' + $("#classes-select").val();
+        var color = $('.color:eq(0)').val();
         $(className).css('background', color);
     });
+
+    $('.paint-button:eq(1)').click(function () {
+        var className = $("#class-input").val();
+        var color = $('.color:eq(1)').val();
+        if (className) {
+            $('.' + className).css('background', color);
+        }
+    });
+    
+    $('#clear-button').click(function() {
+        $('li').css('background', 'none');
+    });
+
 });
---
+++ Level 2/03. Javascript Applications (April 2015)/01. jQuery-Overview/03. TableGenerator/js/tableGenerator.js
@@ -0,0 +1,28 @@
+﻿$(document).ready(function () {
+    $('#generator').click(function () {
+        $("#cars").remove();
+
+        if ($('#input').val() && $.parseJSON($('#input').val())) {
+            var input = $.parseJSON($('#input').val());
+            generateTable();
+
+            function generateTable() {
+                if (input.length) {
+                    var table = $('<table id="cars"><thead><tr><th>Manufacturer</th><th>Model</th><th>Year</th><th>Price</th><th>Class</th></tr></thead><tbody></tbody></table>');
+                    $(input).each(function (index, el) {
+                        var row = '<tr><td>' +
+                            el.manufacturer + '</td><td>' +
+                            el.model + '</td><td>' +
+                            el.year + '</td><td>' +
+                            el.price + '</td><td>' +
+                            el.class + '</td></tr>';
+
+                        table.append($(row));
+                    });
+
+                    table.hide().appendTo($('#wrapper')).fadeIn(1500);
+                }
+            }
+        }
+    });
+});
