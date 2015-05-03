c5a27cbbe64f8233964d23d07c9ea4a148cbf20f
Nikola Nikolov <nikola_00219@abv.bg>
2015-05-03 11:49:28 +0300

    add Products Exam Solution

---
+++ Level 2/03. Javascript Applications (April 2015)/Exam problems/Products/scripts/views/menuView.js
@@ -0,0 +1,26 @@
+var app = app || {};
+app.views = app.views || {};
+
+app.views.menuView = (function(){
+    function menuView(selector, data) {
+        var defer = Q.defer();
+
+        $.get('templates/navigation.html', function(template) {
+            var temp = Handlebars.compile(template);
+            var html = temp(data);
+            $(selector).html(html);
+        }).success(function(_data) {
+            defer.resolve(_data);
+        }).error(function(error) {
+            defer.reject(error);
+        });
+
+        return defer.promise;
+    }
+
+    return {
+        load: function (selector, data) {
+            return menuView(selector, data);
+        }
+    }
+}());
